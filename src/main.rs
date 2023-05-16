ee701e88eb8f4daaa4ff5a639cb8540ab3ee0e9a
Tyler Fanelli <tfanelli@redhat.com>
2023-05-15 21:05:21 -0400

    Implement import subcommand
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 #![deny(clippy::all)]
 
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use anyhow::{anyhow, Context, Result};
 use sev::firmware::host::*;
@@ -26,6 +26,9 @@ enum SnpHostCmd {
 
     #[structopt(about = "Export a certificate chain to a given directory")]
     Export(export::Export),
+
+    #[structopt(about = "Import a certificate chain to the AMD PSP")]
+    Import(import::Import),
 }
 
 fn firmware() -> Result<Firmware> {
@@ -58,6 +61,7 @@ fn main() -> Result<()> {
     match snphost.cmd {
         SnpHostCmd::Show(show) => show::cmd(show),
         SnpHostCmd::Export(export) => export::cmd(export),
+        SnpHostCmd::Import(import) => import::cmd(import),
     }
 }
 
@@ -142,7 +146,7 @@ mod show {
 mod export {
     use super::*;
 
-    use std::{fs, io::Write, result, str::FromStr};
+    use std::{io::Write, result, str::FromStr};
 
     #[derive(StructOpt)]
     pub struct Export {
@@ -238,5 +242,72 @@ mod export {
         }
 
         Ok(())
+    }
+}
+
+mod import {
+    use super::*;
+
+    use std::fs::{read, DirEntry};
+
+    #[derive(StructOpt)]
+    pub struct Import {
+        #[structopt(about = "The directory to find the encoded certificates")]
+        pub path: PathBuf,
+    }
+
+    pub fn cmd(import: Import) -> Result<()> {
+        if !import.path.exists() {
+            return Err(anyhow!(format!(
+                "path {} does not exist",
+                import.path.display()
+            )));
+        }
+
+        let mut table: Vec<CertTableEntry> = vec![];
+
+        for dir_entry in fs::read_dir(import.path.clone())? {
+            match dir_entry {
+                Ok(de) => table_add_entry(de, &mut table)?,
+                Err(_) => {
+                    return Err(anyhow!(format!(
+                        "unable to read directory at path {}",
+                        import.path.display()
+                    )))
+                }
+            }
+        }
+
+        firmware()?.snp_set_ext_config(ExtConfig::new_certs_only(table))?;
+
+        Ok(())
+    }
+
+    fn table_add_entry(de: DirEntry, table: &mut Vec<CertTableEntry>) -> Result<()> {
+        let cert_type = entry_get_type(&de.path())?;
+        let data = read(de.path())?;
+
+        table.push(CertTableEntry::new(cert_type, data));
+
+        Ok(())
+    }
+
+    fn entry_get_type(path: &PathBuf) -> Result<CertType> {
+        let file_name_string = path
+            .file_name()
+            .context(format!("unable to read file at path {:?}", path))?
+            .to_string_lossy()
+            .into_owned();
+
+        let subs: Vec<&str> = file_name_string.split('.').collect();
+        match subs[0] {
+            "ark" => Ok(CertType::ARK),
+            "ask" => Ok(CertType::ASK),
+            "vcek" => Ok(CertType::VCEK),
+            _ => Err(anyhow!(
+                "unable to determine certificate type of path {:?}",
+                path
+            )),
+        }
     }
 }
