734682855ae0e6f6bfc30c4ac8b86d60b8a5ca20
Tyler Fanelli <tfanelli@redhat.com>
2023-07-18 17:36:00 -0400

    reset: Reset SEV-SNP extended config instead of PLATFORM_RESET
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -406,10 +406,11 @@ mod reset {
     use super::*;
 
     pub fn cmd() -> Result<()> {
-        firmware()?
-            .platform_reset()
+        let mut fw = firmware()?;
+
+        fw.snp_reset_config()
             .map_err(|e| anyhow::anyhow!(format!("{:?}", e)))
-            .context("error resetting SEV-SNP platform")
+            .context("error resetting SEV-SNP configuration")
     }
 }
 
