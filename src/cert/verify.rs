5fe42c9c5f54e52d0c04123b978832419db85a79
DGonzalezVillal <Diego.GonzalezVillalobos@amd.com>
2024-02-21 21:56:00 +0000

    Added DER and VLEK support
    Signed-off-by: DGonzalezVillal <Diego.GonzalezVillalobos@amd.com>

--- src/cert/verify.rs
+++ src/cert/verify.rs
@@ -1,32 +1,49 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::{anyhow, Context, Result};
-use std::{fs::read, path::PathBuf};
+use std::{
+    fs::read,
+    path::{Path, PathBuf},
+};
 
 use colorful::*;
 use sev::certs::snp::{ca, Certificate, Chain, Verifiable};
 use structopt::StructOpt;
 
+pub fn find_cert_in_dir(dir: &Path, cert: &str) -> Result<PathBuf, anyhow::Error> {
+    if dir.join(format!("{cert}.pem")).exists() {
+        Ok(dir.join(format!("{cert}.pem")))
+    } else if dir.join(format!("{cert}.der")).exists() {
+        Ok(dir.join(format!("{cert}.der")))
+    } else {
+        return Err(anyhow::anyhow!("{cert} certificate not found in directory"));
+    }
+}
+
 #[derive(StructOpt)]
 pub struct Verify {
-    #[structopt(about = "The path to the ARK certificate")]
-    ark: PathBuf,
-
-    #[structopt(about = "The path to the ASK certificate")]
-    ask: PathBuf,
-
-    #[structopt(about = "The path to the VCEK certificate")]
-    vcek: PathBuf,
+    #[structopt(help = "Path to directory containing certificates.")]
+    pub certs_dir: PathBuf,
 }
 
 pub fn cmd(verify: Verify, quiet: bool) -> Result<()> {
-    let chain = chain(verify)?;
+    let ark_path = find_cert_in_dir(&verify.certs_dir, "ark")?;
+    let ask_path = find_cert_in_dir(&verify.certs_dir, "ask")?;
+    let mut vek_type: &str = "VCEK";
+    let vek_path = match find_cert_in_dir(&verify.certs_dir, "vlek") {
+        Ok(vlek_path) => {
+            vek_type = "VLEK";
+            vlek_path
+        }
+        Err(_) => find_cert_in_dir(&verify.certs_dir, "vcek")?,
+    };
 
-    let (ark, ask, vcek) = (
-        (&chain.ca.ark, "ARK"),
-        (&chain.ca.ask, "ASK"),
-        (&chain.vcek, "VCEK"),
-    );
+    // Get a cert chain from directory
+    let cert_chain = chain(ark_path, ask_path, vek_path, vek_type)?;
+
+    let ark = (&cert_chain.ca.ark, "ARK");
+    let ask = (&cert_chain.ca.ask, "ASK");
+    let vek = (&cert_chain.vek, vek_type);
 
     let mut err = true;
 
@@ -36,7 +53,7 @@ pub fn cmd(verify: Verify, quiet: bool) -> Result<()> {
 
     err |= status(ark, ark, true, quiet);
     err |= status(ark, ask, false, quiet);
-    err |= status(ask, vcek, false, quiet);
+    err |= status(ask, vek, false, quiet);
 
     match err {
         true => Ok(()),
@@ -76,18 +93,18 @@ fn ssym(res: bool, self_signed: bool, signer: &str, signee: &str) -> String {
     }
 }
 
-fn chain(verify: Verify) -> Result<Chain> {
+fn chain(ark_path: PathBuf, ask_path: PathBuf, vek_path: PathBuf, vek_type: &str) -> Result<Chain> {
     Ok(Chain {
         ca: ca::Chain {
-            ark: cert(verify.ark, "ARK")?,
-            ask: cert(verify.ask, "ASK")?,
+            ark: cert(ark_path, "ARK")?,
+            ask: cert(ask_path, "ASK")?,
         },
-        vcek: cert(verify.vcek, "VCEK")?,
+        vek: cert(vek_path, vek_type)?,
     })
 }
 
 fn cert(path: PathBuf, name: &str) -> Result<Certificate> {
-    Certificate::from_pem(
+    Certificate::from_bytes(
         &read(path.clone()).context(format!("unable to read {}", path.display()))?,
     )
     .context(format!(
